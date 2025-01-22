07faa46ca4db75d81f82bbda3e14505abce4d3f4
Jintao <48515469+woshimajintao@users.noreply.github.com>
2025-01-22 01:05:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -186,7 +186,8 @@ function findRehabilitationCenters(location, lat = null, lng = null, useCurrentL
 
     console.log("Request URL:", url); // Log the request URL for debugging
 
-    fetch(`/api/places?url=${encodeURIComponent(url)}`)
+    //fetch(`/api/places?url=${encodeURIComponent(url)}`)
+    fetch(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=drug+rehabilitation&key=${GOOGLE_API_KEY}`)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
